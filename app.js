a9612162a612f2642b09e9c598bb29814f7ec3a2
senektra <rcvallada@gmail.com>
2024-03-01 19:31:56 -0800

    Finish step 4

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ import * as dbfile from './db.json' assert { type: 'json' }
 const app = express()
 const db = dbfile.default
 
-const htmlInfo = `<p>Phonebook has info for ${db.length} people</p>
+app.get('/api/persons', (_req, res) => {
+  res.json(db)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = db.find(person => person.id === id)
+  person ? res.json(person) : res.status(404).send('Person not found')
+})
+
+app.get('/info', (req, res) => {
+  const htmlInfo = `<p>Phonebook has info for ${db.length} people</p>
 <br/>
 <p>${(new Date())}</p>
 `
 
-app.get('/api/persons', (req, res) => {
-  res.json(db)
-})
-
-app.get('/info', (req, res) => {
   res.send(htmlInfo)
 })
 
