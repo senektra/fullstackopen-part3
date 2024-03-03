0af6ec95f26b66c9dc70c918e4bccbafac8d27b9
senektra <rcvallada@gmail.com>
2024-03-02 17:37:11 -0800

    Finish part a exercises

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 import express from 'express'
+import morgan from 'morgan'
+import crypto from 'node:crypto'
 
 import * as dbfile from './db.json' assert { type: 'json' }
 
 const app = express()
-const db = dbfile.default
+let db = dbfile.default
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+app.use(morgan(
+  ':method :url :status :res[content-length] - :response-time ms :body'
+))
+app.use(express.json())
+
+const queryPersonsById = id => db.find(persons => id === persons.id)
+const queryPersonsByName = name => db.find(persons => name.toLowerCase() === persons.name.toLowerCase())
 
 app.get('/api/persons', (_req, res) => {
   res.json(db)
@@ -11,7 +23,7 @@ app.get('/api/persons', (_req, res) => {
 
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
-  const person = db.find(person => person.id === id)
+  const person = queryPersonsById(id)
   person ? res.json(person) : res.status(404).send('Person not found')
 })
 
@@ -22,6 +34,36 @@ app.get('/info', (req, res) => {
 `
 
   res.send(htmlInfo)
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name) {
+    res.status(400).send('Name for contact not specified')
+  }
+  else if (queryPersonsByName(body.name)) {
+    res.status(400).send('Name already exists in phonebook')
+  }
+  else if (!body.number) {
+    res.status(400).send('Number for contact not specified')
+  }
+  else {
+    const newPerson = {
+      id: crypto.randomUUID(),
+      name: body.name,
+      number: body.number
+    }
+
+    db.push(newPerson)
+    res.json(newPerson)
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  db = db.filter(person => person.id !== id)
+  res.status(204).end()
 })
 
 app.listen(3001)
